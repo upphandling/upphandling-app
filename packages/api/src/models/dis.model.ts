b2829613877c1ec0907bf51faec49f10496fcbdf
Christian Landgren <christian@landgren.nu>
2022-01-06 00:08:11 +0100

    feat: added model for dis

---
+++ packages/api/src/models/dis.model.ts
@@ -0,0 +1,72 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Dis extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  title: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  startDate: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  organisation: string;
+
+  @property({
+    type: 'string',
+  })
+  description?: string;
+
+  @property({
+    type: 'string',
+  })
+  status?: string;
+
+  @property({
+    type: 'string',
+  })
+  repo?: string;
+
+  @property({
+    type: 'number',
+  })
+  stars?: number;
+
+  @property({
+    type: 'array',
+    itemType: 'string',
+  })
+  tech?: string[];
+
+  @property({
+    type: 'array',
+    itemType: 'string',
+  })
+  services?: string[];
+
+
+  constructor(data?: Partial<Dis>) {
+    super(data);
+  }
+}
+
+export interface DisRelations {
+  // describe navigational properties here
+}
+
+export type DisWithRelations = Dis & DisRelations;
