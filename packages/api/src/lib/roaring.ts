42cd30b0c658faf4621bee2a306c6d237574f1e5
Christian Landgren <christian@landgren.nu>
2022-01-09 13:47:22 +0100

    feat: use roaring production keys

--- packages/api/src/lib/roaring.ts
+++ packages/api/src/lib/roaring.ts
@@ -1,27 +1,32 @@
 import fetch from 'cross-fetch'
 
 export class Roaring {
-  private static token : {access_token: string}
+  private static token : {access_token: string, expires_in: number}
   public static async getToken(): Promise<{access_token:string}> {
+    if (Roaring.token !== null) return Roaring.token
     const key = process.env.ROARING_KEY || 'e3YCCMVKvA0Pu5lAsmd02hk2xloa' // sandbox
     const secret = process.env.ROARING_SECRET || '0Ly8E04vUWzn534aCNXSKhaMhewa' // sandbox
     const base64 = Buffer.from(`${key}:${secret}`).toString('base64')
-    const token = Roaring.token || await fetch('https://api.roaring.io/token', {
+    const token = await fetch('https://api.roaring.io/token', {
       method: 'POST',
       body: 'grant_type=client_credentials',
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
         Authorization: `Basic ${base64}`
       }
-    }).then(res => res.json())
+    })
+    .then(res => res.json())
     Roaring.token = token
+
+    // remove token automatically after expiry date
+    setTimeout(() => Roaring.token = null!, (token.expires_in ?? 3600) * 1000 )
     return token
   }
 
   public static async lookupCompany(orgnr: string) {
     const roaringToken = await Roaring.getToken()
     const info = await fetch(
-      `https://api.roaring.io/se/company/overview/2.0/history/${orgnr}?fromDate=2016-12-11&toDate=2018-02-21`,
+      `https://api.roaring.io/se/company/overview/2.0/history/${orgnr}`,
       {
         
         headers: {
@@ -29,13 +34,7 @@ export class Roaring {
           'Accept': 'application/json',
           'Content-Type': 'application/json',
       }}
-    ).then((res: any) => {
-      console.dir(res)
-      console.log(res.ok)
-      console.log(res.status)
-      return res.json()
-    }) as any
-
+    ).then((res: any) => res.json()) as any
 
     const latestCompanyRecord = info.records ? info.records[0] : null
     console.log('roaring response', latestCompanyRecord)
