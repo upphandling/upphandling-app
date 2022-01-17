474ec3a1dc21f642401d822117000eafe966893a
Christian Landgren <christian@landgren.nu>
2022-01-17 02:08:32 +0100

    fix: use api.upphandl... instead of localhost

--- packages/Upphandling/api/participation.js
+++ packages/Upphandling/api/participation.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 export const getParticipations = async (disId) => {
-  const { data } = await axios.get(`https://api.upphandling.app/participations?filter[where][disId]=${disId}`)
+  const { data } = await axios.get(`https://api.upphandling.app/dis/${disId}/companies`)
   return data
 }
 
 export const createParticipation = async ({disId, companyId}) => {
-  const { data } = await axios.post(`http://localhost:3000/participations`, {disId, companyId})
+  const { data } = await axios.post(`https://api.upphandling.app/participations`, {disId, companyId})
   return data
 }
