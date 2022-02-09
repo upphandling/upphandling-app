38b3ba943dc9b100fc430db41fcf257122d31b2e
Stefan Wallin <github@stefan-wallin.se>
2022-02-09 21:20:16 +0100

    fix: theme and proper resizing on different screens

--- packages/Upphandling/App.js
+++ packages/Upphandling/App.js
@@ -1,20 +1,33 @@
 import React from 'react'
+import { useColorScheme } from 'react-native'
 import * as eva from '@eva-design/eva'
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components'
 import { EvaIconsPack } from '@ui-kitten/eva-icons'
 import { AppNavigator } from './screens/AppNavigator'
-import {
-  QueryClient, QueryClientProvider,
-} from "react-query"
-import theme from './theme/dark.json'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import darkTheme from './theme/dark.json'
+import lightTheme from './theme/light.json'
+
 const queryClient = new QueryClient()
-export default () => (
-  <>
-    <IconRegistry icons={EvaIconsPack} />
-    <ApplicationProvider {...eva} theme={{...eva.dark, ...theme}}>
-      <QueryClientProvider client={queryClient}>
-        <AppNavigator />
-      </QueryClientProvider>
-    </ApplicationProvider>
-  </>
-)
+const getUIKittenTheme = (colorScheme) => {
+  if (colorScheme === 'dark') {
+    return { ...eva.dark, ...darkTheme }
+  } else {
+    return { ...eva.light, ...lightTheme }
+  }
+}
+
+export default () => {
+  const colorScheme = useColorScheme() || 'dark'
+  const theme = getUIKittenTheme(colorScheme)
+  return (
+    <>
+      <IconRegistry icons={EvaIconsPack} />
+      <ApplicationProvider {...eva} theme={theme}>
+        <QueryClientProvider client={queryClient}>
+          <AppNavigator colorScheme={colorScheme} />
+        </QueryClientProvider>
+      </ApplicationProvider>
+    </>
+  )
+}
